0518c17440f3d81fe7e8ff523464c8593c2028f4
Esteban <n4b3ts3@gmail.com>
2023-02-25 15:54:37 -0500

    feat: Made totally automatic by using envs files in the ~/.github_deploy folder
    Next commit should be about documentations and integrating this with jenkins

--- github_deploy.sh
+++ github_deploy.sh
@@ -1,42 +1,64 @@
 #!/usr/bin/env bash
-## Build
-mkdir -p temp
-echo $@
-zip temp/artifact $@
-# Publish on github
-echo "Publishing on Github..."
-echo -n "Please enter your PAT: "
-read -s token && echo
-# Get the last tag name 
-echo "Getting the last tag name... "
-tag=$(2>/dev/null git describe --tags)
-if $tag; then
-  echo -n "No tag were detected, please set one: "
-  read tag
-  git tag create $tag
-fi 
-# Get the full message associated with this tag
-message="$(2>/dev/null git for-each-ref refs/tags/$tag --format='%(contents)')"
-if $message; then
-  echo "No tag message were available from refs contents, please input one or leave blank to default to latest commit message"
-  echo -n ">>> " && read message
-  if [ -e message ]; then
-    message=$(git log -1 --pretty=%B)
-  fi
-fi
-# Get the title and the description as separated variables
-name=$(echo "$message" | head -n1)
-description=$(echo "$message" | tail -n +3)
-description=$(echo "$description" | sed -z 's/\n/\\n/g') # Escape line breaks to prevent json parsing problems
-# Create a release
-echo "Please enter your username or organization name (as in the url https://github.com/organization_name/): "
-echo -n ">>> " && read username # input here your username
-echo -n "Please enter your repository: "
-read repository
-release=$(curl -XPOST -H "Authorization:token $token" --data "{\"tag_name\": \"$tag\", \"target_commitish\": \"master\", \"name\": \"$name\", \"body\": \"$description\", \"draft\": false, \"prerelease\": true}" https://api.github.com/repos/$username/$repository/releases)
-# Extract the id of the release from the creation response
-id=$(echo "$release" | sed -n -e 's/"id":\ \([0-9]\+\),/\1/p' | head -n 1 | sed 's/[[:blank:]]//g')
-# Upload the artifact
-curl -XPOST -H "Authorization:token $token" -H "Content-Type:application/octet-stream" --data-binary @temp/artifact.zip https://uploads.github.com/repos/$username/$repository/releases/$id/assets?name=artifact.zip
+# author: Esteban Chacon Martin
+# LICENSE: GPL
+# postdata: Enjoy it :)
+#
+# ENVIRONMENT:
+# (str)  GD_USERNAME: The username or organization name, like in the URL https://github.com/organization_name/
+# (str)  PAT: Personal Access Token, we encourage you to use fined granted access ;)
+# (str)  REPOSITORY: The repository that this release is for 
+# (str)  BRANCH: The target commit branch
+# (str)  TAG: For example v1.0.0beta etc.. read in github about tags to know more
+# (str)  DESCRIPTION: The description for this release
+# (bool) PRERELEASE: If it is a prerelease or not 
+# (bool) GEN_PREREL_NOTES: If should generate prerelease notes or no.
+# (bool) DRAFT: I dont know but it is something this is a TODO for later xD 
+
+mkdir -p temp # creates the temp folder in the current directory if not exists. 
+mkdir -p ~/.github_deploy/ # Creates a folder for saving the .env(s) to automatically deploy projects
+
+for file in "$(ls ~/.github_deploy/ | grep -x '.*.env$')" # iterates over each .env file searching for the ones matching our requirements
+do
+  source ~/.github_deploy/$file # load the environment so now we can deploy easily
+  
+  zip temp/artifact $@ # Zip All files given as arguments to this script inside a file called temp/artifact.zip
+  # Publish on github
+  echo "Publishing to github, repo: $REPOSITORY username: $GD_USERNAME..."
+  url="https://api.github.com/repos/$GD_USERNAME/$REPOSITORY/releases"
+  release_json="{ \
+      'tag_name':'$TAG',\
+      'target_commitish':'$BRANCH',\
+      'name':'$TAG',\
+      'body': '$DESCRIPTION', \
+      'draft':$DRAFT, \
+      'prerelease':$PRERELEASE, \
+      'generate_release_notes': $GEN_PREREL_NOTES \
+    } \
+  "
+  release_json=$(replace "'" "\"" <<< $release_json)
+  
+  release=$(\
+  curl \
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $PAT" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    -d "$release_json" \
+    $url
+  )
+  # Extract the id of the release from the creation response
+  id=$(echo "$release" | sed -n -e 's/"id":\ \([0-9]\+\),/\1/p' | head -n 1 | sed 's/[[:blank:]]//g')
+  # Upload the artifact
+  curl \
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $PAT" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    -H "Content-Type: application/octet-stream" \
+    https://uploads.github.com/repos/$GD_USERNAME/$REPOSITORY/releases/$id/assets?name=$REPOSITORY.zip \
+    --data-binary "@temp/artifact.zip"
+  
+done
+
 rm temp/artifact.zip
 rmdir temp 
